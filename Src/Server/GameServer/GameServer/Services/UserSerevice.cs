1b8ba8cf668d1e6f75c62451d17628610c746531
windbell233 <1520057107@qq.com>
2020-08-26 16:53:57 +0800

    用户注册完成

---
+++ Src/Server/GameServer/GameServer/Services/UserSerevice.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Network;
+using SkillBridge.Message;
+using GameServer.Entities;
+
+namespace GameServer.Services
+{
+    class UserService : Singleton<UserService>
+    {
+
+        public UserService()
+        {
+            MessageDistributer<NetConnection<NetSession>>.Instance.Subscribe<UserRegisterRequest>(this.OnRegister);//订阅该消息
+        }
+
+        public void Init()
+        {
+
+        }
+
+        void OnRegister(NetConnection<NetSession> sender, UserRegisterRequest request)//request请求
+        {
+            Log.InfoFormat("UserRegisterRequest: User:{0}  Pass:{1}", request.User, request.Passward);
+
+            NetMessage message = new NetMessage();
+            message.Response = new NetMessageResponse();
+            message.Response.userRegister = new UserRegisterResponse();
+
+            TUser user = DBService.Instance.Entities.Users.Where(u => u.Username == request.User).FirstOrDefault();//查询,服务端校验
+            if (user != null)
+            {
+                message.Response.userRegister.Result = Result.Failed;
+                message.Response.userRegister.Errormsg = "用户已存在.";
+            }
+            else
+            {
+                TPlayer player = DBService.Instance.Entities.Players.Add(new TPlayer());
+                DBService.Instance.Entities.Users.Add(new TUser() { Username = request.User, Password = request.Passward, Player = player });
+                DBService.Instance.Entities.SaveChanges();
+                message.Response.userRegister.Result = Result.Success;
+                message.Response.userRegister.Errormsg = "None";
+            }
+
+            byte[] data = PackageHandler.PackMessage(message);
+            sender.SendData(data, 0, data.Length);
+        }
+    }
+}
